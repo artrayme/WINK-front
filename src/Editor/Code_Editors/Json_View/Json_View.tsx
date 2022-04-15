9fd2906b5379e69ab4fbd8b3ce6dab0d43d83854
artrayme <artrayme@protonmail.com>
2022-04-15 13:48:10 +0300

    feat(json): change library for json-editor and rewrite js to ts

---
+++ src/Editor/Code_Editors/Json_View/Json_View.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import CodeEditor from "../../CodeEditor";
+// import ReactJson from "react-json-view";
+// import {Alignment, ControlGroup, Navbar, NavbarGroup, NumericInput, Tag} from "@blueprintjs/core";
+import JSONInput from 'react-json-editor-ajrm';
+import locale from 'react-json-editor-ajrm/locale/en';
+
+export default class JsonView extends CodeEditor {
+
+    constructor(props) {
+        super(props);
+        this.state = {...this.state};
+    }
+
+    componentDidMount = () => {
+        this.setState({editor: "JsonView"});
+    };
+
+    componentWillUnmount = () => {
+        this.saveEditorDataFromState();
+    };
+
+    updateCode = e => {
+        // const newCode = JSON.stringify(e.updated_src);
+        this.setState({
+            code: e.updated_src
+        });
+        // this.updateDocument(newCode);
+    };
+
+    makeJSON(code) {
+        try {
+            return JSON.parse(code);
+        } catch {
+            return {name: this.state.documentName};
+        }
+    }
+
+    //ToDo config
+    render() {
+        const code = this.makeJSON(this.state.code);
+        return (
+            <JSONInput
+                height="100%"
+                width="100%"
+                // placeholder={JSON.parse("{ToDo: config}")}
+                // placeholder={JSON.stringify("{ToDo: config}")}
+                // locale={locale}
+                // onChange={(e) => {
+                //     this.updateCode(e)
+                // }}
+            />
+        );
+    }
+}
