702f8f00050a155fa79e6348b9108c278a0fe9bb
artrayme <artrayme@protonmail.com>
2022-04-19 20:32:13 +0300

    feat(autocomplete): implement request for autocomplete

---
+++ src/Server/ServerAutocompleter.ts
@@ -0,0 +1,16 @@
+import axios, {AxiosResponse} from "axios";
+import {AutocompleteConfig, ServerConfig} from "../globalConfig";
+
+export function getIdtfs(idtfPart: string): Promise<AxiosResponse> | void {
+    return axios.get(ServerConfig.scWinkBackUri + '/element', {
+        params: {part: idtfPart, limit: AutocompleteConfig.limit},
+        headers: {"Access-Control-Allow-Origin": "*"}
+    })
+        .then(response => {
+            return response.data
+        })
+        .catch(function (error) {
+            // ToDo exception
+            console.log(error);
+        });
+}
