ef24896f90bf7b04a8a5dbd181a391c514a5b0eb
artrayme <artrayme@protonmail.com>
2022-04-26 19:38:00 +0300

    feat(webpack): add webpack config

---
+++ src/webpack.config.js
@@ -0,0 +1,10 @@
+// I'm not sure about the importance of this code
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+module.exports = {
+    plugins: [
+        new MonacoWebpackPlugin({
+            // available options are documented at https://github.com/Microsoft/monaco-editor-webpack-plugin#options
+            languages: ['json']
+        })
+    ]
+};
