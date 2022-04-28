7a4061defecd57fd3270904763215cbcf61c9c14
artrayme <artrayme@protonmail.com>
2022-04-28 14:13:06 +0300

    feat(searcher): add search logic

--- src/LayoutComponents/ScSearcher.tsx
+++ src/LayoutComponents/ScSearcher.tsx
@@ -1,25 +1,32 @@
-import React from "react";
+import React, {Component} from "react";
 import MonacoEditor from "react-monaco-editor";
 import {Button} from "@blueprintjs/core";
+import {find} from "../Server/ServerSearcher";
+import {convertToJson} from "../Utilities/SearcherJsonParser/SearcherConverter";
+
+export interface ScSearcherState {
+    code: string;
+}
 
 
-export class ScSearcher extends React.Component {
-    code: string
+export class ScSearcher extends Component<any, ScSearcherState> {
+    code: string = ""
 
     constructor(props) {
         super(props);
+        this.state = {
+            code: ""
+        }
     }
 
     editorDidMount(editor, monaco) {
-        console.log('editorDidMount', editor);
         editor.focus();
     }
 
     onChange(newValue, e) {
         console.log('onChange', newValue, e);
-    }
-
-    convert(code: string) {
+        // this.setState({code = newValue})
+        this.setState({code: newValue});
 
     }
 
@@ -31,7 +38,7 @@ export class ScSearcher extends React.Component {
         return (
             <div>
                 <Button onClick={() => {
-
+                    find(convertToJson(this.code));
                 }}>Find</Button>
                 <MonacoEditor
                     width="800"
@@ -40,7 +47,7 @@ export class ScSearcher extends React.Component {
                     theme="vs-dark"
                     value={this.code}
                     options={options}
-                    onChange={this.onChange}
+                    onChange={(val) => this.code = val}
                     editorDidMount={this.editorDidMount}
                 />
             </div>
