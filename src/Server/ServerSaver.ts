78aad68638adfd72d36ffb2633b7667968924916
artrayme <artrayme@protonmail.com>
2022-05-03 19:25:26 +0300

    feat(saver): add functions for save and delete scs

--- src/Server/ServerSaver.ts
+++ src/Server/ServerSaver.ts
@@ -1,8 +1,29 @@
 import axios from "axios";
 import {GlobalState, ServerConfig} from "../globalConfig";
+import {convertToJson} from "../Utilities/ScsToJsonConverter";
 
 export function saveScs(code: string, name: string) {
+    let jsonCode = convertToJson(code).slice(1)
+    let request = {
+        filename: name,
+        nativeFormat: "scs",
+        scElements: jsonCode
+    }
+    console.log(request)
 
+    axios({
+        method: 'post',
+        url: ServerConfig.scWinkBackUri + '/element',
+        data: request
+    }).then(response => {
+        GlobalState.updateViewportFunction(response.data)
+    });
+}
+
+export function deleteScs(name: string) {
+    axios.delete(ServerConfig.scWinkBackUri + '/element', {params: {name: name}}).then(response => {
+        GlobalState.updateViewportFunction("")
+    });
 }
 
 export function saveRdf(code: string, name: string) {
